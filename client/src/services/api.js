6314ee331ee0062cd7a31c45501ff697d6c7c66a
HikariAkiraa <muhammaddhiya2006@gmail.com>
2026-04-30 16:35:08 +0700

    update api baseURL to railway

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://project-api-production-fcc9.up.railway.app',
   headers: {
     'Cache-Control': 'no-cache',
   },
